897deeddd73d5faaca991c8c340a8050951584be
shy <1529110749@qq.com>
2024-07-30 11:29:29 +0800

    调整主页样式

--- mbti-test-mini/src/pages/index/index.tsx
+++ mbti-test-mini/src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import {Image, View} from "@tarojs/components";
 import {AtButton} from "taro-ui";
+import Taro from "@tarojs/taro";
 
 import "taro-ui/dist/style/components/button.scss"; // 按需引入
 import headerBg from "../../assets/headerBg.jpg";
@@ -16,10 +17,29 @@ export default () => {
       <View className="at-article__h2 subTitle">
         只需2分钟，就能非常准确地描述出你是谁，以及你的性格特点
       </View>
-      <AtButton type="primary" circle className="enterBtn">
+      <AtButton
+        type="primary"
+        circle
+        className="enterBtn"
+        onClick={() => {
+          Taro.navigateTo({
+            url: "/pages/doQuestion/index",
+          });
+        }}
+      >
         开始测试
       </AtButton>
-      <Image className="headerBg" src={headerBg} />
+      <Image
+        className="headerBg"
+        style={{
+          width: "100%",
+          position: "fixed",
+          bottom: "16px",
+          textAlign: "center"
+        }}
+        mode="widthFix"
+        src={headerBg}
+      />
       <GlobalFooter />
     </View>
   );
