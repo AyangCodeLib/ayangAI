aa32eea0aa0c4e50382e7d5e7d24cce6200e2487
shy <1529110749@qq.com>
2024-07-30 10:55:54 +0800

    完善问答功能

--- mbti-test-mini/src/pages/doQuestion/index.tsx
+++ mbti-test-mini/src/pages/doQuestion/index.tsx
@@ -1,39 +1,90 @@
 import {View} from "@tarojs/components";
 import {AtButton, AtRadio} from "taro-ui";
 import "taro-ui/dist/style/components/button.scss"; // 按需引入
+import {useEffect, useState} from "react";
+import Taro from "@tarojs/taro";
+
 import "./index.scss";
 import GlobalFooter from "../../components/GlobalFooter";
 import questions from "../../data/questions.json";
-import {useState} from "react";
 
 /**
  * 做题页面
  */
 export default () => {
-  const question = questions[0];
-  const questionOptions: any = question.options.map((option) => {
-    return { label: `${option.key}. ${option.value}`, value: option.key };
-  });
   // 当前题目序号（从1开始）
   const [current, setCurrent] = useState<number>(1);
+  // 当前题目
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const questionOptions: any = currentQuestion.options.map((option) => {
+    return { label: `${option.key}. ${option.value}`, value: option.key };
+  });
+
+  // 当前答案
+  const [currentAnswer, setCurrentAnswer] = useState<string>();
+  // 回答列表
+  const [answerList] = useState<string[]>([]);
+
+  // 序号变化时，切换当前题目和当前回答
+  useEffect(() => {
+    setCurrentQuestion(questions[current - 1]);
+    setCurrentAnswer(answerList[current - 1]);
+  }, [answerList, current]);
 
   return (
     <View className="doQuestionPage">
-      <View className="at-article__h1 title">
-        {current}、{question.title}
+      <View className="at-article__h2 title">
+        {current}、{currentQuestion.title}
       </View>
       <View className="options-wrapper">
-        <AtRadio options={questionOptions} />
+        <AtRadio
+          options={questionOptions}
+          value={currentAnswer}
+          onClick={(value) => {
+            setCurrentAnswer(value);
+            // 记录答案
+            answerList[current - 1] = value;
+          }}
+        />
       </View>
-      <AtButton type="primary" circle className="controlBtn">
-        下一题
-      </AtButton>
-      <AtButton type="primary" circle className="controlBtn">
-        查看结果
-      </AtButton>
-      <AtButton circle className="controlBtn">
-        上一题
-      </AtButton>
+      {current < questions.length && (
+        <AtButton
+          type="primary"
+          circle
+          className="controlBtn"
+          disabled={!currentAnswer}
+          onClick={() => setCurrent(current + 1)}
+        >
+          下一题
+        </AtButton>
+      )}
+      {current == questions.length && (
+        <AtButton
+          type="primary"
+          circle
+          className="controlBtn"
+          disabled={!currentAnswer}
+          onClick={() => {
+            // 传递答案
+            Taro.setStorageSync("answerList", answerList);
+            // 跳转到结果页面
+            Taro.navigateTo({
+              url: "/pages/result/index",
+            });
+          }}
+        >
+          查看结果
+        </AtButton>
+      )}
+      {current > 1 && (
+        <AtButton
+          circle
+          className="controlBtn"
+          onClick={() => setCurrent(current - 1)}
+        >
+          上一题
+        </AtButton>
+      )}
       <GlobalFooter />
     </View>
   );
